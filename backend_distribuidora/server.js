b6fa5052a8d5a3860eef487d7277d5293eaab4f6
Yomyom25 <131711625+Yomyom25@users.noreply.github.com>
2025-11-27 14:30:52 -0600

    Update server.js

--- backend_distribuidora/server.js
+++ backend_distribuidora/server.js
@@ -1750,6 +1750,7 @@ function generarReporteVentasPDF(doc, ventas, fechaDesde, fechaHasta) {
         y += 15;
     });
 
+    
     // Pie de página
     doc.fontSize(8)
         .font('Helvetica')
